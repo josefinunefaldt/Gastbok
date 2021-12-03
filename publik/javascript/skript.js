600cf9d0add0de34b434b8d83cdb500d9f9ff1cc
josefinunefaldt <92858498+josefinunefaldt@users.noreply.github.com>
2021-12-03 09:04:00 +0000

    uppdatering av inmat
    uppdaterade och la till så alla fält från användarens inmatningar finns med

--- publik/javascript/skript.js
+++ publik/javascript/skript.js
@@ -1,33 +1,48 @@
 
+
 function inlagg() {
 // hämtar data från servern
 let forfragan = new XMLHttpRequest(); // skapa nytt request
 forfragan.open('GET', './text.json', true); // öppna min jsonfil
 
 forfragan.onload = function () {  // när resultatet laddats.
+   
 
 let data = JSON.parse(this.response); // tolka JSON-svaret
 for(let i = data.length-1; i>=0; i--){
 
-const postName = document.createElement("p");
-postName.setAttribute("class","postName");
-postName.textContent = data[i].namn;
-document.body.appendChild(postName);
+const fNamn = document.createElement("p");
+fNamn.setAttribute("class","fNamn");
+fNamn.textContent = data[i].namn;
+document.body.appendChild(fNamn);
 
-const enamn = document.createElement("p");
-enamn.setAttribute("class","enamn");
-enamn.textContent = data[i].efternamn;
-document.body.appendChild(enamn);
+const eNamn = document.createElement("p");
+eNamn.setAttribute("class","eNamn");
+eNamn.textContent = data[i].efternamn;
+document.body.appendChild(eNamn);
 
 const nummer = document.createElement("p");
 nummer.setAttribute("class","nummer");
 nummer.textContent = data[i].telefonnummer;
 document.body.appendChild(nummer);
 
+const kommentar = document.createElement("p");
+kommentar.setAttribute("class","kommentar");
+kommentar.textContent = data[i].kommentar;
+document.body.appendChild(kommentar);
+const minDiv = document.createElement("div");
+minDiv.setAttribute("class","minDiv")
+document.body.appendChild(minDiv)
+
+
+
 
    
 }
 }
+
 forfragan.send();
+
 }
+
 
