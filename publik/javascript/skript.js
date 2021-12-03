c1ac32f5381416ec9509751724ad8d48d0e3cac9
josefinunefaldt <92858498+josefinunefaldt@users.noreply.github.com>
2021-12-03 15:24:53 +0000

    regEx
    Lagt till en onchange function där jag validerar inmatningarna

--- publik/javascript/skript.js
+++ publik/javascript/skript.js
@@ -1,48 +1,77 @@
-
-
 function inlagg() {
-// hämtar data från servern
-let forfragan = new XMLHttpRequest(); // skapa nytt request
-forfragan.open('GET', './text.json', true); // öppna min jsonfil
+    // hämtar data från servern
+    let forfragan = new XMLHttpRequest(); // skapa nytt request
+    forfragan.open("GET", "./text.json", true); // öppna min jsonfil
 
-forfragan.onload = function () {  // när resultatet laddats.
-   
+    forfragan.onload = function () {
+        // när resultatet laddats.
 
-let data = JSON.parse(this.response); // tolka JSON-svaret
-for(let i = data.length-1; i>=0; i--){
+        let data = JSON.parse(this.response); // tolka JSON-svaret
 
-const fNamn = document.createElement("p");
-fNamn.setAttribute("class","fNamn");
-fNamn.textContent = data[i].namn;
-document.body.appendChild(fNamn);
+        for (let i = data.length - 1; i >= 0; i--) {
+            const fNamn = document.createElement("p");
+            fNamn.setAttribute("class", "fNamn");
+            fNamn.textContent = data[i].namn;
+            document.body.appendChild(fNamn);
 
-const eNamn = document.createElement("p");
-eNamn.setAttribute("class","eNamn");
-eNamn.textContent = data[i].efternamn;
-document.body.appendChild(eNamn);
+            const eNamn = document.createElement("p");
+            eNamn.setAttribute("class", "eNamn");
+            eNamn.textContent = data[i].efternamn;
+            document.body.appendChild(eNamn);
 
-const nummer = document.createElement("p");
-nummer.setAttribute("class","nummer");
-nummer.textContent = data[i].telefonnummer;
-document.body.appendChild(nummer);
+            const nummer = document.createElement("p");
+            nummer.setAttribute("class", "nummer");
+            nummer.textContent = data[i].telefonnummer;
+            document.body.appendChild(nummer);
 
-const kommentar = document.createElement("p");
-kommentar.setAttribute("class","kommentar");
-kommentar.textContent = data[i].kommentar;
-document.body.appendChild(kommentar);
-const minDiv = document.createElement("div");
-minDiv.setAttribute("class","minDiv")
-document.body.appendChild(minDiv)
+            const kommentar = document.createElement("p");
+            kommentar.setAttribute("class", "kommentar");
+            kommentar.textContent = data[i].kommentar;
+            document.body.appendChild(kommentar);
+            const minDiv = document.createElement("div");
+            minDiv.setAttribute("class", "minDiv");
+            document.body.appendChild(minDiv);
+        }
+    };
 
-
-
-
-   
-}
+    forfragan.send();
 }
 
-forfragan.send();
+function regexTest(regex, variabelTest) {
+    let bokstavTest = regex.test(variabelTest);
 
+    if (bokstavTest === true) {
+        return true;
+    }
+
+    if (bokstavTest === false) {
+        return false;
+    }
 }
 
+function onChange() { 
+    let inmatForNamn = document.getElementById("forNamn").value;
+    let aterkopplingForNamn = document.getElementById("utskriftFornamn");
+    let inmatEfterNamn = document.getElementById("efterNamn");
+    let aterkopplingEfterNamn = document.getElementById("utskriftEfterNamn");
 
+    if (regexTest(/[a-ö]+i/, inmatForNamn) == true) {
+        aterkopplingForNamn.innerHTML = "godkänt";
+        
+        console.log("rad 62");
+    } else {
+        aterkopplingForNamn.innerHTML = "fel";
+        console.log("rad 65");
+       
+    }
+
+    if (regexTest(/[a-ö]+i/, inmatEfterNamn) == true) {
+        aterkopplingEfterNamn.innerHTML = "godkänt";
+        
+        console.log("rad 72");
+    } else {
+        aterkopplingEfterNamn.innerHTML = "fel";
+        console.log("rad 75");
+       
+    }
+}
